b2f3912a613c63a1416a02941eede39dd2e06f7b
Ji Zhou <calf.zhou@gmail.com>
2015-01-16 09:24:16 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+autoproxy-gfwlist
+=================
+
+A mirror of https://code.google.com/p/autoproxy-gfwlist/ (http://autoproxy-gfwlist.googlecode.com/svn/).
+
+In case if you cannot load [the official gfwlist.txt](http://autoproxy-gfwlist.googlecode.com/svn/trunk/gfwlist.txt), use this one:
+
+* https://raw.githubusercontent.com/calfzhou/autoproxy-gfwlist/master/gfwlist.txt
+
+or the one in `trunk` branch:
+
+* https://raw.githubusercontent.com/calfzhou/autoproxy-gfwlist/trunk/gfwlist.txt
+
+----
+
+Don't like this README file? Switch to [trunk branch](https://github.com/calfzhou/autoproxy-gfwlist/tree/trunk), which is exactly the same with official svn repo.
---
+++ fetch_and_merge.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+# Fetches remote svn updates to local repository,
+# and then merge these update into local master branch and trunk branch.
+# Finally, push all local changes to GitHub.
+
+# Exit immediately if a command exits with a non-zero status.
+# Print commands and their arguments as they are executed.
+set -ex
+
+if [ -z "$GFWLIST_HOME" ]; then
+    GFWLIST_HOME=/Users/zhouji/projects/autoproxy-gfwlist
+fi
+
+max_retry=5
+failure_idle=10
+proxy_duration=120
+
+function fetch_svn_updates() {
+    local i=0
+    local ret=0
+    while true; do
+        i=`expr $i + 1`
+        proxychains4 git svn fetch
+        ret=$?
+        if [ $ret -eq 0 ]; then
+            break
+        elif [ $i -lt $max_retry ]; then
+            sleep $failure_idle
+        else
+            break
+        fi
+    done
+    return $ret
+}
+
+cd "$GFWLIST_HOME"
+
+git checkout master
+
+ssh -D 7070 -f -p 22 gocalfco@gocalf.com sleep $proxy_duration
+
+set +e
+if fetch_svn_updates; then
+    :
+else
+    exit $?
+fi
+set -e
+
+git merge origin/trunk -m 'auto merge svn trunk branch'
+
+git checkout trunk
+git merge --ff-only origin/trunk
+
+git push --all
+
+git checkout master
